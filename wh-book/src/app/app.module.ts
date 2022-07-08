74cf2062bc16d004edfc6c5ebc2cab62079d7929
akaydo <tecno4kaa@mail.ru>
2022-07-08 08:29:04 +0300

    Added ErrorPage and routes

--- wh-book/src/app/app.module.ts
+++ wh-book/src/app/app.module.ts
@@ -3,13 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HeaderComponent } from 'src/components/header/header.component';
 import { ContentComponent } from 'src/components/content/content.component';
 import { PaginationComponent } from 'src/components/pagination/pagination.component';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponent } from '../pages/first/first.component';
 import { SecondComponent } from '../pages/second/second.component';
 import { ThirdComponent } from '../pages/third/third.component';
 import { FourthComponent } from '../pages/fourth/fourth.component';
 import { FifthComponent } from '../pages/fifth/fifth.component';
+import { ErrorpageComponent } from '../pages/errorpage/errorpage.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'app',
+  children: [
+    {
+      path: 'pages', children: [
+        { path: '', component: FirstComponent },
+        { path: '1', component: FirstComponent },
+        { path: '2', component: SecondComponent },
+        { path: '3', component: ThirdComponent },
+        { path: '4', component: FourthComponent },
+        { path: '5', component: FifthComponent }
+      ]},
+    { path: '**', component: ErrorpageComponent }
+  ]},
+  {path: '**', component: ErrorpageComponent}
+]
 
 @NgModule({
   declarations: [
@@ -21,13 +39,25 @@ import { FifthComponent } from '../pages/fifth/fifth.component';
     SecondComponent,
     ThirdComponent,
     FourthComponent,
-    FifthComponent
+    FifthComponent,
+    ErrorpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    [RouterModule.forRoot(routes)]
   ],
   providers: [],
+  exports: [
+    AppComponent,
+    HeaderComponent,
+    PaginationComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent,
+    FifthComponent,
+    RouterModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
