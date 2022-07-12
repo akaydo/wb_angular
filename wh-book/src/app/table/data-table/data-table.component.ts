a78d082266aba09d598d468a1e8fc9414c69fc39
akaydo <tecno4kaa@mail.ru>
2022-07-12 14:16:01 +0300

    some changes table

--- wh-book/src/app/table/data-table/data-table.component.ts
+++ wh-book/src/app/table/data-table/data-table.component.ts
@@ -4,70 +4,12 @@ import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
 import { LiveAnnouncer } from '@angular/cdk/a11y';
 
-@Component({
-  selector: 'app-data-table',
-  templateUrl: './data-table.component.html',
-  styleUrls: ['./data-table.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
-})
-
-export class DataTableComponent implements AfterViewInit {
-  extendedData = [
-    {position: 24, name: 'ElementName', weight: 881.9950604495106, displayedInCell: 'Is not gas'},
-    {position: 18, name: 'ElementName', weight: 797.1882834817152, displayedInCell: 'Is not gas'},
-    {position: 81, name: 'ElementName', weight: 83.27795127138148, displayedInCell: 'Is not gas'},
-    {position: 12, name: 'ElementName', weight: 484.7582920570088, displayedInCell: 'Is not gas'},
-    {position: 37, name: 'ElementName', weight: 97.37565904722523, displayedInCell: 'Is not gas'},
-    {position: 42, name: 'ElementName', weight: 807.5197617071963, displayedInCell: 'Is not gas'},
-    {position: 87, name: 'ElementName', weight: 82.77845613163004, displayedInCell: 'Is not gas'},
-    {position: 10, name: 'ElementName', weight: 203.494457177799, displayedInCell: 'Is not gas'},
-    {position: 73, name: 'ElementName', weight: 228.65816441155462, displayedInCell: 'Is not gas'},
-    {position: 6, name: 'ElementName', weight: 767.8192773812831, displayedInCell: 'Is not gas'}
-  ]
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  columnsToDisplay = ['position','name', 'weight', 'symbol'];
-  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
-  expandedElement: PeriodicElement | null | undefined;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'displayedInCell'];
-
-  constructor(private _liveAnnouncer: LiveAnnouncer) {
-    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
-  }
-
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatTable) table: MatTable<PeriodicElement>;
-
-  ngAfterViewInit() {
-    this.dataSource.sort = this.sort;
-  }
-
-  announceSortChange(sortState: Sort) {
-    if (sortState.direction) {
-      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
-    } else {
-      this._liveAnnouncer.announce('Sorting cleared');
-    }
-  }
-
-  addData() {
-    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-    ELEMENT_DATA.push(ELEMENT_DATA[randomElementIndex]);
-    this.table.renderRows();
-    this.dataSource.data = ELEMENT_DATA;
-  }
-}
-
 export interface PeriodicElement {
   position: number;
   name: string;
   weight: number;
   symbol: string;
+  extendedData?: any[];
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
@@ -132,5 +74,70 @@ const ELEMENT_DATA: PeriodicElement[] = [
     symbol: 'Ne',
   },
 ];
+
+let extendedData = [
+  {position: 24, name: 'ElementName', weight: 881.9950604495106, displayedInCell: 'Is not gas'},
+  {position: 18, name: 'ElementName', weight: 797.1882834817152, displayedInCell: 'Is not gas'},
+  {position: 81, name: 'ElementName', weight: 83.27795127138148, displayedInCell: 'Is not gas'},
+  {position: 12, name: 'ElementName', weight: 484.7582920570088, displayedInCell: 'Is not gas'},
+  {position: 37, name: 'ElementName', weight: 97.37565904722523, displayedInCell: 'Is not gas'},
+  {position: 42, name: 'ElementName', weight: 807.5197617071963, displayedInCell: 'Is not gas'},
+  {position: 87, name: 'ElementName', weight: 82.77845613163004, displayedInCell: 'Is not gas'},
+  {position: 10, name: 'ElementName', weight: 203.494457177799, displayedInCell: 'Is not gas'},
+  {position: 73, name: 'ElementName', weight: 228.65816441155462, displayedInCell: 'Is not gas'},
+  {position: 6, name: 'ElementName', weight: 767.8192773812831, displayedInCell: 'Is not gas'}
+]
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+
+export class DataTableComponent implements AfterViewInit {
+  dataSource = new MatTableDataSource<PeriodicElement>;
+  columnsToDisplay = ['position','name', 'weight', 'symbol'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement: PeriodicElement | null | undefined;
+  displayedExtendedColumns: string[] = ['position', 'name', 'weight', 'displayedInCell'];
+  constructor(private _liveAnnouncer: LiveAnnouncer) {
+    ELEMENT_DATA.forEach(el => {
+      el.extendedData = extendedData
+    })
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+    
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatTable) table: MatTable<PeriodicElement>;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+    ELEMENT_DATA.push(ELEMENT_DATA[randomElementIndex]);
+    this.table.renderRows();
+    this.dataSource.data = ELEMENT_DATA;
+  }
+}
+
+
 
 
